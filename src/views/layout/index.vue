93f748377192a9809a1505f01e2b0cf822e46f2d
zhuxiaona <zhuxiaona@example.com>
2020-02-01 14:14:53 +0800

    顶部导航，底部导航完成布局

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,15 +1,51 @@
 <template>
-  <div>
-    布局
-    <router-view></router-view>
+  <div class="container">
+    <van-nav-bar v-if="showNavBar" fixed title="天天知道" right-text="搜索"></van-nav-bar>
+    <!-- div控制定位 -->
+    <div class="my-wrapper" :class="{noTop:!showNavBar}">
+      <router-view></router-view>
+    </div>
+    <van-tabbar v-model="active" route>
+      <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item to="/question" icon="chat-o">问答</van-tabbar-item>
+      <van-tabbar-item to="/video" icon="video-o">视频</van-tabbar-item>
+      <van-tabbar-item to="/user" icon="user-o">我的</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'layout',
+  computed: {
+    showNavBar () {
+      return this.$route.path !== '/user'
+    }
+  },
+  data () {
+    return {
+      active: 0
+    }
+  }
+
 }
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
+.container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  .my-wrapper {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    padding-top: 46px;
+    padding-bottom: 50px;
+    box-sizing: border-box;
+    &.noTop {
+      padding-top: 0;
+    }
+  }
+}
 </style>
