2a720f9976fc8db5c2aea3df197ff754ca8f85f6
zhuxiaona <zhuxiaona@example.com>
2020-02-06 10:34:56 +0800

    完成编辑资料基础布局

--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -1,12 +1,38 @@
 <template>
-  <div class="container">
-    <van-nav-bar title="编辑资料" left-arrow right-text="保存" @click-left="$router.back()"></van-nav-bar>
+ <div class="container">
+    <van-nav-bar left-arrow @click-left="$router.back()" title="编辑资料" right-text="保存" ></van-nav-bar>
+    <van-cell-group>
+      <van-cell is-link title="头像"  center>
+        <van-image
+          slot="default"
+          width="1.5rem"
+          height="1.5rem"
+          fit="cover"
+          round
+          src="https://img.yzcdn.cn/vant/cat.jpeg"
+        />
+      </van-cell>
+      <van-cell is-link title="名称" :value="user.name" />
+      <van-cell is-link title="性别" :value="user.gender ===0?'男':'女'"/>
+      <van-cell is-link title="生日" :value="user.birthday" />
+    </van-cell-group>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'profile'
+  name: 'profile',
+  data () {
+    return {
+      user: {
+        name: '',
+        gender: 0,
+        birthday: '2019-08-08'
+      }
+    }
+  },
+  methods: {
+  }
 }
 </script>
 
