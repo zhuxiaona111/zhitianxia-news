bb10f687df5c13ba3c8a93e25d1397243c7feb0f
zhuxiaona <zhuxiaona@example.com>
2020-02-06 11:19:25 +0800

    修改资料的弹出层布局

--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -10,12 +10,32 @@
           fit="cover"
           round
           src="https://img.yzcdn.cn/vant/cat.jpeg"
+          @click="showPhoto=true"
         />
       </van-cell>
-      <van-cell is-link title="名称" :value="user.name" />
-      <van-cell is-link title="性别" :value="user.gender ===0?'男':'女'"/>
-      <van-cell is-link title="生日" :value="user.birthday" />
+      <van-cell @click="showName=true" is-link title="名称" :value="user.name" />
+      <van-cell @click="showGender=true" is-link title="性别" :value="user.gender ===0?'男':'女'"/>
+      <van-cell @click="showBirthday=true" is-link title="生日" :value="user.birthday" />
     </van-cell-group>
+    <!-- 头像弹层 -->
+    <van-popup v-model="showPhoto" style="width:80%">
+      <van-cell is-link title="本地相册"></van-cell>
+      <van-cell is-link title="拍照"></van-cell>
+    </van-popup>
+    <!-- 昵称弹层 -->
+    <van-popup v-model="showName" style="width:80%">
+      <van-field label="名字"  type="textarea" v-model="user.name"  rows="4"></van-field>
+    </van-popup>
+    <!-- 性别选择弹层 -->
+    <van-action-sheet :actions="actions" cancel-text="取消" v-model="showGender"></van-action-sheet>
+  <!-- 日期选择弹层 -->
+  <van-popup v-model="showBirthday" position="bottom">
+    <van-datetime-picker
+  v-model="currentDate"
+  type="date"
+  :min-date="minDate"
+/>
+  </van-popup>
   </div>
 </template>
 
@@ -24,11 +44,19 @@ export default {
   name: 'profile',
   data () {
     return {
+      minDate: new Date(1900, 1, 1), // 最小时间
+      // maxDate: new Date(2020, 10, 1), // 最大时间
+      currentDate: new Date(), // 当前时间
+      showPhoto: false, // 是否显示显示头像编辑弹层
+      showName: false, // 是否显示编辑昵称弹层
+      showGender: false,
+      showBirthday: false,
       user: {
-        name: '',
+        name: '123',
         gender: 0,
         birthday: '2019-08-08'
-      }
+      },
+      actions: [{ name: '男' }, { name: '女' }]
     }
   },
   methods: {
