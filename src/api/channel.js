42fc95e0a9217e63c27909659e94396b60d63d7e
zhuxiaona <zhuxiaona@example.com>
2020-02-21 19:25:37 +0800

    设置本数据存储

--- src/api/channel.js
+++ src/api/channel.js
@@ -1,8 +1,23 @@
 import request from '@/utils/request'
+import store from '@/store'
+const CACHE_CHANNEL_T = 'hm-91-toutiao-t' // 游客缓存的key
+const CACHE_CHANNEL_U = 'hm-91-toutiao-u'
 // 获取频道内容
 export function getChannel () {
-  return request({
-    url: '/user/channels'
+  return new Promise(async function (resolve, reject) {
+    // 做我们的逻辑
+    let key = store.state.user.token ? CACHE_CHANNEL_U : CACHE_CHANNEL_T // 用于缓存的key
+    // 从缓存中读取数据
+    let str = localStorage.getItem(key) // 得到缓存结果
+    if (str) {
+      //  如果str存在 表示缓存中有数据
+      resolve({ channels: JSON.parse(str) }) // 表示从缓存中获取的数据 通过执行链下发给下一个promise
+    } else {
+      // 如果没有数据
+      const data = await request({ url: '/user/channels' }) // 从线上拉取数据
+      localStorage.setItem(key, JSON.stringify(data.channels)) // 将线上数据写入缓存
+      resolve(data) // 将线上获取的数据释放下给promise
+    }
   })
 }
 // 渲染可选频道
